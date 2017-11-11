7eab51f0e6bc3ba3fb5eabbd6c13be5b26c19b61
Shashi <liyanagemadushani21@gmail.com>
2017-11-10 22:22:09 -0500

    iOS App Initialized

---
+++ Iosapp/insightTEK/insightTEK/SearchViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SearchViewController.swift
+//  insightTEK
+//
+//  Created by Madushani Lekam Wasam Liyanage on 11/10/17.
+//  Copyright © 2017 Madushani Lekam Wasam Liyanage. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+
+    @IBOutlet var mentorTableView: UITableView!
+    let searchBar = UISearchBar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mentorTableView.dataSource = self
+        mentorTableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Iosapp/insightTEK/insightTEK/SigninViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SigninViewController.swift
+//  insightTEK
+//
+//  Created by Madushani Lekam Wasam Liyanage on 11/10/17.
+//  Copyright © 2017 Madushani Lekam Wasam Liyanage. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func signinTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
